87947477b580e37dc5a1ea9b665a65614787778c
Joel Ekeng <joelekeng23@gmail.com>
2023-09-07 07:34:50 +0000

    Object value increment using a function

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,19 +1,9 @@
-#!/usr/bin/node
 const myObject = {
     type: 'object',
-    value: 12
-  };
-  console.log(myObject);
+    value: 12,
+    incr: function() {
+        this.value++;
+    }
+};
 
-  function incr(myobject){
-    objectvalue = myObject.value;
-    objectvalue++;
-  }
-
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  
+console.log(myObject);
