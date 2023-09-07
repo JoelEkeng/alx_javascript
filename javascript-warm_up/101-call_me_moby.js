413b9cbe6c8058e2e32e8be5091144cfd1badfe0
Joel Ekeng <joelekeng23@gmail.com>
2023-09-07 06:45:18 +0000

    Function that prints another function based on X

---
+++ javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+function callMeMoby(x, thefunction){
+    let i;
+    for (i = 1; 1<=x; i++){
+        console.log(thefunction);
+    }
+} 
+
+module.exports = {
+    callMeMoby,
+};
